7b71b041d6d79ffe99143032bc362ec84c61aef4
Carmelo Bongiorno <c.bongiorno@reply.it>
2024-02-23 14:27:45 +0100

    add correct values of vm

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_windows_virtual_machine" "example" {
   size                  = "Standard_D2s_v3"
   admin_username        = "adminuser"
   admin_password        = "Password1234!"
-  network_interface_ids = ["/subscriptions/0d6ce570-7813-445e-bb22-e35faf195918/resourceGroups/rg-bongiorno-weu-001/providers/Microsoft.Network/networkInterfaces/bongiorno-vm02416_z1",]
+  network_interface_ids = ["/subscriptions/f357b9f3-aeca-4bd9-b1e2-e8a9db3e9374/resourceGroups/rg-bongiorno-weu-001/providers/Microsoft.Network/networkInterfaces/bongiorno-vm02416_z1",]
 
   os_disk {
     caching              = "ReadWrite"
