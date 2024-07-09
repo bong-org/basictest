a6978a5fee3656820565040004ecb33d83b17547
Carmelo Bongiorno <c.bongiorno@reply.it>
2024-07-09 17:28:59 +0200

    add

--- Script/replace.ps1
+++ Script/replace.ps1
@@ -7,17 +7,17 @@ param (
 $containerExists = az storage container exists --account-name $storageAccount --name $containerName --query exists
 
 if ($containerExists -eq $false) {
-    $createContainer = Read-Host "Il container $containerName non esiste. Vuoi crearlo? (s/n)"
-    if ($createContainer -eq "s") {
+    $createContainer = Read-Host "This container $containerName not exists. Do you want create? (y/n)"
+    if ($createContainer -eq "y") {
         az storage container create --account-name $storageAccount --name $containerName
     } else {
-        Write-Host "Operazione annullata."
+        Write-Host "Operation aborted."
         exit
     }
 }
 
 # Append file al container
-$files = Get-ChildItem -Path "./path/to/your/files"
+$files = Get-ChildItem -Path "./AppendFiles"
 foreach ($file in $files) {
     az storage blob upload --account-name $storageAccount --container-name $containerName --file $file.FullName --name $file.Name
 }
