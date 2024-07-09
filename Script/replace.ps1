91e86b3a289d2d46acb836891c0a28e30ae5caa8
Carmelo Bongiorno <c.bongiorno@reply.it>
2024-07-09 18:12:56 +0200

    add

--- Script/replace.ps1
+++ Script/replace.ps1
@@ -1,22 +1,48 @@
+# param (
+#     [string]$storageAccount = $env:AZURE_STORAGE_ACCOUNT,
+#     [string]$containerName = $env:AZURE_CONTAINER_NAME
+# )
+
+# # Verifica se il container esiste
+# $containerExists = az storage container exists --account-name $storageAccount --name $containerName --query exists
+
+# if ($containerExists -eq $false) {
+#     $createContainer = Read-Host "This container $containerName not exists. Do you want create? (y/n)"
+#     if ($createContainer -eq "y") {
+#         az storage container create --account-name $storageAccount --name $containerName
+#     } else {
+#         Write-Host "Operation aborted."
+#         exit
+#     }
+# }
+
+# # Append file al container
+# $files = Get-ChildItem -Path "./AppendFiles"
+# foreach ($file in $files) {
+#     az storage blob upload --account-name $storageAccount --container-name $containerName --file $file.FullName --name $file.Name --overwrite
+# }
+
+
 param (
     [string]$storageAccount = $env:AZURE_STORAGE_ACCOUNT,
     [string]$containerName = $env:AZURE_CONTAINER_NAME
 )
 
-# Verifica se il container esiste
-$containerExists = az storage container exists --account-name $storageAccount --name $containerName --query exists
 
-if ($containerExists -eq $false) {
-    $createContainer = Read-Host "This container $containerName not exists. Do you want create? (y/n)"
-    if ($createContainer -eq "y") {
+# Verifica se il container esiste
+$containerExists = az storage container exists --account-name $storageAccount --name $containerName --query "exists" --output tsv
+
+if ($containerExists -eq "false") {
+    $createContainer = Read-Host "Il container $containerName non esiste. Vuoi crearlo? (s/n)"
+    if ($createContainer -eq "s") {
         az storage container create --account-name $storageAccount --name $containerName
     } else {
-        Write-Host "Operation aborted."
+        Write-Host "Operazione annullata."
         exit
     }
 }
 
-# Append file al container
+# Append file al container con sovrascrittura
 $files = Get-ChildItem -Path "./AppendFiles"
 foreach ($file in $files) {
     az storage blob upload --account-name $storageAccount --container-name $containerName --file $file.FullName --name $file.Name --overwrite
