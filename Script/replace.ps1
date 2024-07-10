8771c6fec6bbd01ea89cf64a6e6b51a4f82fba15
Carmelo Bongiorno <c.bongiorno@reply.it>
2024-07-10 10:22:06 +0200

    add

--- Script/replace.ps1
+++ Script/replace.ps1
@@ -60,37 +60,37 @@
 
 
 param (
-    [string]$configFile = "./path/to/your/config.json"
+    [string]$configFile = "./Config/parameters.json"
 )
 
-# Leggi il file JSON e estrai i valori
-Write-Host "Lettura del file di configurazione JSON: $configFile"
+# Read JSON file and extract values
+Write-Host "Read configuration JSON file: $configFile"
 $config = Get-Content -Raw -Path $configFile | ConvertFrom-Json
 $storageAccount = $config.AZURE_STORAGE_ACCOUNT
 $containerName = $config.AZURE_CONTAINER_NAME
 
-# Log delle variabili lette
-Write-Host "Account di archiviazione: $storageAccount"
-Write-Host "Nome del container: $containerName"
+# Log of variables
+Write-Host "Storage account name: $storageAccount"
+Write-Host "Container name: $containerName"
 
 if (-not $containerName) {
-    Write-Host "Errore: Il nome del container non è specificato."
+    Write-Host "Error: Missing container name."
     exit 1
 }
 
-Write-Host "Verifica se il container $containerName esiste..."
+Write-Host "Check if container $containerName exists..."
 $containerExists = az storage container exists --account-name $storageAccount --name $containerName --query "exists" --output tsv
-Write-Host "Esito verifica container: $containerExists"
+Write-Host "Result of check: $containerExists"
 
 if ($containerExists -eq "false") {
-    Write-Host "Il container $containerName non esiste. Creazione del container..."
+    Write-Host "The container $containerName not exists. Container creation..."
     az storage container create --account-name $storageAccount --name $containerName --enable-delete-retention=false
 }
 
-Write-Host "Caricamento dei file nel container $containerName..."
+Write-Host "Uploading files into the Container $containerName..."
 $files = Get-ChildItem -Path "./AppendFiles"
 foreach ($file in $files) {
-    Write-Host "Caricamento del file: $($file.FullName)"
+    Write-Host "Uploading the file: $($file.FullName)"
     az storage blob upload --account-name $storageAccount --container-name $containerName --file $file.FullName --name $file.Name --overwrite
 }
-Write-Host "Operazione completata."
+Write-Host "Operation completed."
